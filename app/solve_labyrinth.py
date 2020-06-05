88f711af4333e433196868cec360c9451c83b24b
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-06-05 13:47:16 +0200

    Generalize the folder names in run file

--- app/solve_labyrinth.py
+++ app/solve_labyrinth.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 
 import click
+from stable_baselines.common import BaseRLModel
+
 from stable_baselines.common.callbacks import CheckpointCallback, CallbackList
 from stable_baselines import HER, DDPG, SAC, PPO2
 
@@ -10,32 +12,39 @@ from two_blocks_env.labyrinth_env import Labyrinth
 from two_blocks_env.toy_labyrinth_env import ToyLab
 from utils import latest_model
 
-experiment_name = "her-sac-toylab"
-results_dir = Path("../all-results")/experiment_name
-models_dir = results_dir/"ckpts"
-tensorboard_dir = results_dir/"tensorboard"
-model_fpath = lambda: models_dir/latest_model(models_dir)
+
+class Dirs:
+    def __init__(self, experiment_name: str):
+        self.prefix = experiment_name
+        results = Path("../all-results")/experiment_name
+        self.models = results/"ckpts"
+        self.tensorboard = results/"tensorboard"
+
+    @property
+    def best_model(self):
+        return self.models/latest_model(self.models)
 
 
-def train():
+def train(model_class: type(BaseRLModel), dirs: Dirs):
     num_timesteps = 100000
     num_checkpoints = 4
     env = ToyLab()
-    cb = CheckpointCallback(save_freq=num_timesteps//num_checkpoints, save_path=models_dir, name_prefix=experiment_name)
-
-    if os.path.isdir(models_dir):
-        model = HER.load(model_fpath(), env=env, tensorboard_log=tensorboard_dir)
+    cb = CheckpointCallback(save_freq=num_timesteps//num_checkpoints, save_path=dirs.models, name_prefix=dirs.prefix)
+    options = {"env": env, "tensorboard_log": dirs.tensorboard}
+    if os.path.isdir(dirs.models):
+        model = model_class.load(load_path=dirs.best_model, **options)
     else:
-        model = HER('MlpPolicy', env, model_class=SAC, tensorboard_log=tensorboard_dir,
-                    verbose=1)
+        if model_class == HER:
+            options.update(model_class=SAC)
+        model = model_class(policy='MlpPolicy', verbose=1, **options)
     model.learn(total_timesteps=num_timesteps, callback=cb)
 
 
-def viz():
+def viz(model_class: type(BaseRLModel), dirs: Dirs):
     env = ToyLab()
-    model = HER.load(load_path=model_fpath(), env=env)
-    print(f"Loaded model {model_fpath()}")
-    agent = lambda obs: model.predict(obs)[0]
+    model = model_class.load(load_path=dirs.best_model, env=env)
+    print(f"Loaded model {dirs.best_model}")
+    agent = lambda obs: model.predict(obs, deterministic=True)[0]
     run = lambda g: sum(1 for _ in trajectory(agent, env, goal=g, sleep_secs=0.1))
     while True:
         env.render()
@@ -44,11 +53,14 @@ def viz():
 
 
 @click.command()
-@click.option('--do-train', default=False)
-def main(do_train: bool):
+@click.option("--do-train", is_flag=True, help="Whether to train the agent")
+@click.option("--alg", type=click.Choice(['her-sac', 'ppo']), required=True, help="Algorithm: 'her' (HER+SAC) or 'ppo' available.")
+def main(do_train: bool, alg: str):
+    model_class = HER if alg == "her-sac" else PPO2
+    dirs = Dirs(experiment_name=f"{alg}-toylab")
     if do_train:
-        train()
-    viz()
+        train(model_class=model_class, dirs=dirs)
+    viz(model_class=model_class, dirs=dirs)
 
 
 if __name__ == '__main__':
