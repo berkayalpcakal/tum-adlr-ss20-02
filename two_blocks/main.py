91840ce578cbb5d2e66932c69f7bbd8a4d78ed71
Berkay Alp Cakal <berkay.cakal@tum.de>
2020-06-15 15:29:57 +0200

    add initial trainings with random goals

--- two_blocks/main.py
+++ two_blocks/main.py
@@ -1,6 +1,6 @@
 import warnings
 from GenerativeGoalLearning import initialize_GAN, update_and_eval_policy, \
-    label_goals, train_GAN, sample
+    label_goals, train_GAN, sample, eval_policy
 from ppo_agent import PPOAgent
 from two_blocks_env.collider_env import dim_goal
 import numpy as np
@@ -24,9 +24,15 @@ for i in iterations:
 
 #### PARAMETERS ####
 # The rest of the params are in goalGAN.py
-iterations                 = 200 #1000
-num_samples_goalGAN_goals  = 20
-num_samples_from_old_goals = 10
+initial_iterations         = 5
+iterations                 = 1000
+num_samples_goalGAN_goals  = 60
+num_samples_from_old_goals = num_samples_goalGAN_goals // 2
+num_samples_random_goals   = num_samples_goalGAN_goals // 2
+
+SEED = 1000 #random.randint(0,2**32-1)
+np.random.seed(SEED)
+torch.set_default_dtype(torch.float64)
 ####################
 
 env  = ToyLab()
@@ -34,11 +40,24 @@ env  = ToyLab()
 goalGAN   = initialize_GAN(env=env)
 goals_old = torch.Tensor([env.starting_obs]) + torch.randn(num_samples_from_old_goals, dim_goal(env))*0.1
 
-for i in range(iterations):
+## Initial training of the policy with random goals
+goals_plot = None
+for i in range(initial_iterations):
+    rand_goals = torch.tensor(np.random.uniform(-1, 0, size=(num_samples_random_goals, 2)))
+    π,returns  = update_and_eval_policy(rand_goals, π, env)
+    print(f"Average reward: {(sum(returns) / len(returns)):.2f}")
+    labels     = label_goals(returns)
+    print(f"Percentage of 0 vs 1 labels: {[round(n, 2) for n in (np.bincount(labels) / len(labels))]}")
+    display_goals(rand_goals.detach().numpy(), returns, i, env, fileNamePrefix='_')
+    goalGAN    = train_GAN(rand_goals, labels, goalGAN)
+
+
+for i in range(initial_iterations, iterations):
     print(f"\n### BEGIN ITERATION {i} ###")
     z          = torch.randn(size=(num_samples_goalGAN_goals, goalGAN.Generator.noise_size))
     gan_goals  = goalGAN.Generator.forward(z).detach()
-    goals      = torch.cat([gan_goals, sample(goals_old, k=num_samples_from_old_goals)])
+    rand_goals = torch.tensor(np.random.uniform(-1, 1, size=(num_samples_random_goals, 2)))
+    goals      = torch.cat([gan_goals, sample(goals_old, k=num_samples_from_old_goals), rand_goals])
     π, returns = update_and_eval_policy(goals, π, env)
     display_goals(goals.detach().numpy(), returns, i, env)
     print(f"Average reward: {(sum(returns) / len(returns)):.2f}")
