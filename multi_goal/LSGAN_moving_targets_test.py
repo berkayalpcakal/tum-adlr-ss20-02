64571c7ca653346f0ac392d6b8dd729aa9a0a047
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-07-10 09:23:06 +0200

    Add boundary to moving LSGAN test

--- multi_goal/LSGAN_moving_targets_test.py
+++ multi_goal/LSGAN_moving_targets_test.py
@@ -1,5 +1,3 @@
-from random import shuffle
-
 import numpy as np
 import torch
 from scipy.stats import multivariate_normal
@@ -9,19 +7,19 @@ from multi_goal.GenerativeGoalLearning import train_GAN
 from multi_goal.LSGAN import LSGAN
 from multi_goal.utils import get_updateable_contour
 
-####
 start_point = np.array([-0.25, -0.50])
 num_samples = 100
 num_noise_samples = num_samples // 2
 num_mixtures = 4
 eps = 0.2
 
+
 def create_target_trajectory():
     # for simple U-shaped maze
     delta = 0.01
 
-    trajectory = []; angles = []
-    trajectory.append(start_point); angles.append(np.array([0.567]))
+    trajectory = [start_point]
+    angles = [np.array([0.567])]
 
     # 1. bottom-left to bottom-right
     while trajectory[-1][0] < 0.50:
@@ -40,14 +38,15 @@ def create_target_trajectory():
 
     return trajectory, angles
 
+
 def cirlce_coords(n_mixture, radius=0.4, center=start_point, rotation=0.567):
     thetas = np.linspace(0, 2 * np.pi//2, n_mixture, endpoint=False) + rotation
     xs, ys = center[0] + radius * np.sin(thetas), center[1] + radius * np.cos(thetas)
     return xs, ys
 
 
-def multimodal_sample(batch_size, xs, ys, n_mixture=8, std=0.003):
-    ms = [multivariate_normal(mean=[x, y], cov=std) for x, y in zip(xs, ys)]
+def multimodal_sample(batch_size, xs, ys, n_mixture=8, cov=0.003):
+    ms = [multivariate_normal(mean=[x, y], cov=cov) for x, y in zip(xs, ys)]
     from_which_dist = np.random.randint(0, n_mixture, batch_size)
     return np.array([ms[idx].rvs() for idx in from_which_dist])
 
@@ -55,8 +54,8 @@ def multimodal_sample(batch_size, xs, ys, n_mixture=8, std=0.003):
 def do_label(target_pts, train_data, eps):
     labels    = np.zeros(shape=(train_data.shape[0]))
     tree      = KDTree(target_pts)
-    dist, idx =tree.query(train_data)
-    labels[np.argwhere(dist < eps)[:,0].reshape(-1)] = 1
+    dist, idx = tree.query(train_data)
+    labels[np.argwhere(dist < eps)[:, 0].reshape(-1)] = 1
     print("Ratio of positive samples; {}".format(np.sum(labels)/train_data.shape[0]))
 
     return labels
@@ -74,10 +73,10 @@ if __name__ == '__main__':
 
     pts_scatter = ax.scatter([], [], c="orange")
     gan_scatter = ax.scatter([], [], c="blue")
+    boundary_circles = [ax.add_artist(plt.Circle((0, 0), eps, color="orange", fill=False)) for _ in range(num_mixtures)]
 
     gan_pts = G(torch.randn(num_samples, 4)).detach().numpy()
-    for i in range(len(trajectory)):
-        circle_center = trajectory[i]; angle = angles[i]
+    for circle_center, angle in zip(trajectory, angles):
         xs, ys = cirlce_coords(num_mixtures, center=circle_center, rotation=angle)
         target_pts = multimodal_sample(num_samples, xs, ys, num_mixtures)
         noise = np.random.uniform(-1, 1, size=(num_noise_samples, 2))
@@ -87,7 +86,10 @@ if __name__ == '__main__':
         gan_pts = G(torch.randn(num_samples, 4)).detach().numpy()
 
         contour_fn(gan_pts, ax)
-        pts_scatter.set_offsets(target_pts)
+        show_gan_pts = False
+        gan_scatter.set_offsets(gan_pts) if show_gan_pts else pts_scatter.set_offsets(target_pts)
+        [circle.set_center((x, y)) for circle, x, y in zip(boundary_circles, xs, ys)]
+
         fig.tight_layout()
         fig.canvas.draw()
         fig.canvas.flush_events()
