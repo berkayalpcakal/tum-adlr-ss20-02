6a6a8097d6c0a7e9a67b17b7a506a68fcba8c706
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-07-10 16:52:52 +0200

    Migrate out ARC into another repo. Export test suite

--- arc/parallel-solve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for n in $(seq $1); do
-  LOG="logs/out-${n}.log"
-  python solve_labyrinth.py --alg=her-sac --env=bullet_hard --mode=train --num_steps=500000 --seed=$n --random_starts &>"${LOG}" &
-done
