ce4bb94345d5ed7cafcf907e23d96c340f5ee970
Berkay Alp Cakal <berkay.cakal@tum.de>
2020-08-03 00:27:55 +0200

    seed argument passing via click added to main.py

--- multi_goal/main.py
+++ multi_goal/main.py
@@ -1,14 +1,21 @@
 from multi_goal.agents import PPOAgent, GoalGANAgent, HERSACAgent, EvaluateCallback
 from multi_goal.envs.toy_labyrinth_env import ToyLab
+import click
 
 
-if __name__ == '__main__':
-    seed  = 0
+@click.command()
+@click.option("--seed", required=True, type=int)
+def cmd_main(*args, **kwargs):
+    main(*args, **kwargs)
+
+def main(seed: int):
     env   = ToyLab(seed=seed)
-    π     = PPOAgent(env=env, experiment_name="goalgan-ppo", rank=seed)
+    π     = PPOAgent(env=env, experiment_name="goalgan-ppo-seed-{}".format(seed), rank=seed)
     #π     = HERSACAgent(env=env, experiment_name="goalgan-her-sac", rank=seed)
     agent = GoalGANAgent(env=env, agent=π)
 
-    callback = EvaluateCallback(agent=agent, eval_env=ToyLab(seed=seed))
+    callback = EvaluateCallback(agent=agent, eval_env=ToyLab(seed=seed), rank=seed)
     agent.train(timesteps=int(1e6), callbacks=[callback])
 
+if __name__ == '__main__':
+    cmd_main()
