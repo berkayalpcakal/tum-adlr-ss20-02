99c28ce651396862a18ded8471ab5c6a89d44e36
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-08-02 22:41:22 +0200

    Add evaluation to Panda training

--- multi_goal/panda_main.py
+++ multi_goal/panda_main.py
@@ -1,14 +1,22 @@
+import click
 from more_itertools import consume
 
 from multi_goal.GenerativeGoalLearning import trajectory
-from multi_goal.agents import HERSACAgent
+from multi_goal.agents import HERSACAgent, EvaluateCallback
 from multi_goal.envs.pybullet_panda_robot import PandaEnv
 
-if __name__ == '__main__':
-    do_train = True
+
+@click.command()
+@click.option("--do-train", is_flag=True, default=False)
+def main(do_train: bool):
     env = PandaEnv(visualize=not do_train)
     agent = HERSACAgent(env=env)
     if do_train:
-        agent.train(timesteps=100000)
+        cb = EvaluateCallback(agent=agent, eval_env=PandaEnv())
+        agent.train(timesteps=50000, callbacks=[cb])
     while True:
         consume(trajectory(agent, env))
+
+
+if __name__ == '__main__':
+    main()
